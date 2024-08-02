f5cb4c6fce2e4eb50edc0a13701d1951b8e12a6a
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-08-02 10:51:16 +0530

    web

---
+++ 18web/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://www.google.com/"
+
+func main() {
+	fmt.Println("Google web server")
+	
+	response, err := http.Get(url)
+
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Printf("The type of the response is %T\n", response)
+	defer response.Body.Close() // caller's responsibility to close connection
+
+	databytes, err := ioutil.ReadAll(response.Body) // ioutil majorly use for reading
+
+	if err != nil{
+		panic(err)
+	}
+
+	content := string(databytes)
+	fmt.Println(content)
+}
