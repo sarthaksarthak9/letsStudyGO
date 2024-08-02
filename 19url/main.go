3dd99ab719ef55eb776f5ced4ba52779ef5ce190
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-08-02 23:00:07 +0530

    urk

---
+++ 19url/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&payment=ghj45jhj"
+
+func main() {
+	fmt.Println("Welcome to handling urls in golang")
+	fmt.Println(myurl)
+
+	// parsing (parsing - extract some info)
+
+	result, _ := url.Parse(myurl) // url is lib
+
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Port())
+	fmt.Println(result.Path)
+	fmt.Println(result.RawQuery)
+	fmt.Println(result.Host)
+
+	qparams := result.Query();
+
+	fmt.Printf("The type of qparams: %T\n", qparams)
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams{
+		fmt.Println("The params: ",val)
+	} 
+
+	partsOfURL := &url.URL{ // pass a reference thats y &
+		Scheme: "https",
+		Host: "lco.dev",
+		Path: "tutcss" ,
+		RawPath: "user=Sarthak",
+	}
+
+	anotherUrl := partsOfURL.String()
+
+	fmt.Println(anotherUrl)
+}
