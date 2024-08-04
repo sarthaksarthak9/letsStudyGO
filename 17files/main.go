c00c8b91a40d381a939c9170991b4481f63dcd01
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-08-04 17:07:29 +0530

    file handling

--- 17files/main.go
+++ 17files/main.go
@@ -12,11 +12,11 @@ func main() {
 	content := "This should go in a file - LearCodeOnline.in"
 
 	file, err := os.Create("./mylcogofile.txt") // os is a package for file handling in go
-	
+
 	checkNilError(err)
-	
+
 	length, err := io.WriteString(file, content) // this return length (read docs)
-	
+
 	checkNilError(err)
 	fmt.Println("The length is: ", length)
 	defer file.Close() // important to close file, use defer
@@ -31,8 +31,8 @@ func readFile(filename string) {
 	fmt.Println("Text from the file \n", string(dataByte))
 }
 
-func checkNilError(err error){
-	if err != nil{
+func checkNilError(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
