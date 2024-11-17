da6bec433c3b3cf176bda6f4769ea9227e2313fd
sarthaksarthak9 <sarthaknegi908@gmail.com>
2024-11-17 09:59:52 +0530

    deleted:    testing/01bdd/code/adt.go 	deleted:    testing/01bdd/test/adt_test.go 	deleted:    testing/02testify/code/adt.go 	deleted:    testing/02testify/code/go.mod 	modified:   testing/02testify/test/adt_test.go

--- testing/02testify/code/go.mod
+++
@@ -1,3 +0,0 @@
-module adt.go
-
-go 1.22.5
--- testing/02testify/test/adt_test.go
+++ testing/02testify/test/adt_test.go
@@ -24,3 +24,27 @@ func (s *StackSuite) TestIsEmpty() {
 	s.stack.Bury("red")
 	s.False(s.stack.IsEmpty())
 }
+
+func (s *StackSuite) TestUnburySingleTest(){
+	s.stack.Bury("blue")
+
+	s.stack.Unbury()
+	s.Zero(0, s.stack.Size())
+	s.True(s.stack.IsEmpty())
+}
+func (s *StackSuite) TestBury(){
+	s.stack.Bury("red")
+	s.stack.Bury("yellow")
+
+	s.Equal(2, s.stack.Size()) // Equal could compare any alphanumeric char
+}
+
+func (s *StackSuite) TestUnBury(){
+	s.stack.Bury("red")
+	s.stack.Bury("blue")
+
+	s.Equal("red", s.stack.Unbury())
+	s.Equal(1, s.stack.Size())
+}
+
+
