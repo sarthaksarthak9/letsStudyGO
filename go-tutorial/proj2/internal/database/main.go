c9017a365edf0f92e2d5b121e10b609e11c9e96f
sarthaksarthak9 <sarthaknegi908@gmail.com>
2026-01-29 22:27:01 +0530

    db

---
+++ go-tutorial/proj2/internal/database/main.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// to initialize
+func InitDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5440 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Unable to written database")
+		return nil
+	}
+
+	return db
+}
